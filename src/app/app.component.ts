5b5c4baa528ed8c4bd973b592ec50d5a0d5c85f2
pavithra-srit <pavithra.srit@gmail.com>
2026-06-10 12:15:40 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatSidenavContent } from '@angular/material/sidenav';
+import { RouterLink } from '@angular/router';
+import {MatListItem} from '@angular/material/list';
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [MatListModule, RouterModule, MatListItem,
+    MatSidenavModule,MatIconButton,MatIcon,MatSidenavContent,RouterLink],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent {
+
+}
